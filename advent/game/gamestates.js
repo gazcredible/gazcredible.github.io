fb8d082dac77c3b7ad216698e4b947af476aa863
gareth <gareth_lewis@yahoo.com>
2023-11-04 16:20:01 +0000

    .

--- advent/game/gamestates.js
+++ advent/game/gamestates.js
@@ -41,7 +41,7 @@ class GameState_Test extends StateMachineState
 
         let date1 = new Date(Date.now());
         //let date2 = new Date("03/07/2020");
-        let date2 = new Date("10/9/2025");
+        let date2 = new Date("6/4/2026");
 
         let Difference_In_Time = date2.getTime() - date1.getTime();
 
