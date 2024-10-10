9473a2557dbcc6dd343ef583207670e3af34f50e
gazcredible <gareth_lewis@yahoo.com>
2024-10-10 09:03:37 +0100

    Update gamestates.js

--- advent/game/gamestates.js
+++ advent/game/gamestates.js
@@ -41,7 +41,7 @@ class GameState_Test extends StateMachineState
 
         let date1 = new Date(Date.now());
         //let date2 = new Date("03/07/2020");
-        let date2 = new Date("9/4/2027");
+        let date2 = new Date("5/4/2026");
 
         let Difference_In_Time = date2.getTime() - date1.getTime();
 
